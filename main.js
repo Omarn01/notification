73d0342f7caf83eb912739afe8050d5feb9607dc
Omar Nurbagndov <omarnurbagandov0103@gmail.com>
2024-07-03 16:04:10 +0300

    a

---
+++ main.js
@@ -0,0 +1,45 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>Hello Vite!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+  </div>
+`
+
+setupCounter(document.querySelector('#counter'))
+
+const button = document.querySelector('#push')
+const time = document.querySelector('#time')
+
+button.addEventListener('click', () => {
+  Notification.requestPermission().then(perm => {
+    if (perm === 'granted') {
+      new Notification('Eeee')
+    }
+  })
+})
+
+time.addEventListener('click', () => {
+  setTimeout(() => {
+    Notification.requestPermission().then(perm => {
+      if (perm === 'granted') {
+        new Notification('Eeee')
+      }
+    })
+  }, 2000)
+})
